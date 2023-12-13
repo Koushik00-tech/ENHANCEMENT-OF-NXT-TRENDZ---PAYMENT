e5dd7610417e7f2904e576aa0a47df2973e681ad
koushik00-tech <koushik.koppolu00@gmail.com>
2023-12-13 02:45:33 +0000

    NextTrendz-ccbp-kk

---
+++ src/components/Cart/index.js
@@ -0,0 +1,35 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const showEmptyView = cartList.length === 0
+      // TODO: Update the functionality to remove all the items in the cart
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <CartListView />
+                {/* TODO: Add your code for Cart Summary here */}
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
